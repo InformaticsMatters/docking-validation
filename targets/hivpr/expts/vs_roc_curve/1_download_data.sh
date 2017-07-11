0bb2d823bcacb500c12b9b07447a796574b526ad
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2017-07-11 11:52:08 +0100

    fixing download paths

--- targets/hivpr/expts/vs_roc_curve/1_download_data.sh
+++ targets/hivpr/expts/vs_roc_curve/1_download_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 wget https://www.dropbox.com/sh/3cz5gnr8hz79kfa/AACO9h0jm7tOgEe78of5WA6Sa/hivpr_all_results.sd.gz?dl=0 -O hivpr_all_results.sd.gz
-wget https://www.dropbox.com/sh/3cz5gnr8hz79kfa/AACO9h0jm7tOgEe78of5WA6Sa/decoys.txt?dl=0 -O decoys.txt
-wget https://www.dropbox.com/sh/3cz5gnr8hz79kfa/AACO9h0jm7tOgEe78of5WA6Sa/ligands.txt?dl=0 -O ligands.txt
+wget https://www.dropbox.com/sh/3cz5gnr8hz79kfa/AACO-DV2mTlmv6LWdK9-iuyoa/decoys.txt?dl=0 -O decoys.txt
+wget https://www.dropbox.com/sh/3cz5gnr8hz79kfa/AAD-BlqPMGQ1ZGofh3YMcX0Za/ligands.txt?dl=0 -O ligands.txt
 
 
 
