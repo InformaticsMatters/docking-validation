a2c43e91f7048835668208b68b91b57c61caa29b
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2020-03-20 11:43:36 +0000

    correct workflow executed

--- targets/nudt7/expts/jackall/5_run_jackall_local.sh
+++ targets/nudt7/expts/jackall/5_run_jackall_local.sh
@@ -6,6 +6,6 @@ for path in work/NUDT7A-*
 do
 	echo "Processing $path ..."
 	cd $path
-    nextflow -c ../../nextflow.config run ../../main.nf -with-docker -with-report -with-trace $@
+    nextflow -c ../../nextflow.config run ../../../../../../scripts/transfs/transfs.nf -with-docker -with-report -with-trace --prmfile docking-global.prm --asfile docking-global.as
     cd $basedir
 done
